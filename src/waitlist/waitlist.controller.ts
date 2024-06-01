2ee42dd678e644fe353ecb96e8446bdc312bf8f0
ayowilfred95 <38457345+ayowilfred95@users.noreply.github.com>
2024-06-01 19:00:04 +0100

    create database schema and database connection established successfully

--- src/waitlist/waitlist.controller.ts
+++ src/waitlist/waitlist.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('waitlist')
-export class WaitlistController {}
+export class WaitlistController {
+    @Get()
+    getWaitlist(){
+        return [{Message:"welcome to nestjs waitlist"}]
+    }
+}
